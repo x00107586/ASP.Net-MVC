9c13695102a5daf14bfb6500cdae180a0804b196
x00107586 <x00107586@gmail.com>
2017-03-27 13:25:17 +0100

    Add files via upload

---
+++ CalculatorApp/Controllers/CalcController.cs
@@ -0,0 +1,39 @@
+﻿using CalculatorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Calculate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calc)
+        {
+            if(ModelState.IsValid)
+            {
+                calc.Result = calc.Answer();
+                return RedirectToAction("Confirm", calc);
+            }
+            else
+            {
+                return View(calc);
+            }
+        }
+
+        public ActionResult Confirm(Calculator calc)
+        {
+            return View(calc);
+        }
+
+
+    }
+}
---
+++ CalculatorApp/Models/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Models
+{
+    public enum Operators
+    {
+        [Display(Name = " + ")]
+        add,
+        [Display(Name = " - ")]
+        subtract,
+        [Display(Name = " * ")]
+        multiply,
+        [Display(Name = " / ")]
+        divide,
+    }
+
+    public class Calculator
+    {
+        [Required(ErrorMessage = "Required field")]
+        [Display(Name = "Number A")]
+        public double NumA { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [Display(Name = "Number B")]
+        public double NumB { get; set; }
+
+        [Display(Name = "Operator")]
+        public Operators Operator { get; set; }
+
+        public double Result { get; set; }
+
+        public double Answer()
+        {
+            
+                double answer = 0;
+                if(Operator.ToString() == "add")
+                {
+                    answer = NumA + NumB;
+                }
+                else if(Operator.ToString() == "subtract")
+                {
+                    answer = NumA - NumB;
+                }
+                else if (Operator.ToString() == "multiply")
+                {
+                    answer = NumA * NumB;
+                }
+                else if (Operator.ToString() == "divide")
+                {
+                    answer = NumA / NumB;
+                }
+                return answer;
+           
+                
+        } 
+    }
+}
